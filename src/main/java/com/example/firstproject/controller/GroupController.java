d676c20e7675c042cf54d9b1124d3f3047185a2d
kaameo <kaameo12@gmail.com>
2024-07-10 22:26:37 +0900

    Added function for viewing comment lists on web pages. (chapter 16)

---
+++ src/main/java/com/example/firstproject/controller/GroupController.java
@@ -0,0 +1,27 @@
+package com.example.firstproject.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+public class GroupController {
+    @Autowired
+    private GroupService groupService;
+    @Autowired
+    private TeamService teamService;
+
+    @GetMapping("/groups/{groupId}")
+    public String show(@PathVariable Long groupId, Model model){
+        // 해당 메서드를 완성하세요
+        GroupDto groupDto = groupService.getGroup(groupId);
+        List<TeamDto> teamDtos = teamService.getTeamOnGroup(groupId);
+        model.addAttribute("groupname",groupDto.getName());
+        model.addAttribute("teamDtoList", teamDtos);
+        return "groups/show";
+    }
+}
